0cfbce7cf9a483ea0d01c18333d03eb365ae57ea
Matias Ferreria <82279633+PhoenixBlue-afk@users.noreply.github.com>
2026-05-12 07:45:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ let imagen3=document.getElementById("foto3")
 
 let counter = 0;
 
-
+/*let creoEtiquetas = ()=>{
+  let verificador= true
+  if (counter==) {
+    
+  }
+}*/
 function moverBotonAleatorio() {
   
   
@@ -30,29 +35,24 @@ function moverBotonAleatorio() {
   btn.style.position = "absolute"; // O 'fixed'
   btn.style.left = posx + "px";
   btn.style.top = posy + "px";
+
    if (counter >= 3 && counter<6 ) {
-  relleno.remove()
-  imagen.remove() 
-  imagen1.src= "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjNtZzJkZTIwejQxaHpxdGQ3cmk1YXc0MjF2ZDRic29kZHJ0YXd2eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7AzEXdIb1wyCTWJntb/giphy.gif"
-  relleno1.innerHTML=""
-    relleno1.append("¿Me estas rechazando? 💔")
+  relleno.innerHTML = ""
+  imagen.src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjNtZzJkZTIwejQxaHpxdGQ3cmk1YXc0MjF2ZDRic29kZHJ0YXd2eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7AzEXdIb1wyCTWJntb/giphy.gif" 
+    relleno.append("¿Me estas rechazando? 💔")
 
   /*Cara de gatito triste*/
 
 } else if (counter >= 6 && counter<=10) {
-  relleno1.remove()
-  imagen1.remove()
-    relleno2.append("¿Estas jugando con mi corazon?")
-    
-    imagen2.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG82NHlwYjJ3b3ViMWZzdDFyZ215NnlvYWlhejIwNHJpbzE5bTVkMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LgR0AnXJBrO4E/giphy.gif"
-   
+  relleno.innerHTML = ""
+  imagen.src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG82NHlwYjJ3b3ViMWZzdDFyZ215NnlvYWlhejIwNHJpbzE5bTVkMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LgR0AnXJBrO4E/giphy.gif" 
+
+    relleno.append("¿Estas jugando con mi corazon?")   
 }
 else if(counter >10){
-  relleno2.remove()
-  imagen2.remove()
-  relleno3.innerHTML=""
-  relleno3.append("Solo cerra la pesataña voy a llorar 😭")
-  imagen3.src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnBlZWQ4aW1mb3phYmpoa3FsN3BmY2sxeW5tbWxvZzhpZ3VldTgwdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/EQIkgfMCjs1kk/giphy.gif"
+  relleno.innerHTML=""
+  relleno.append("Solo cerra la pesataña voy a llorar 😭")
+  imagen.src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnBlZWQ4aW1mb3phYmpoa3FsN3BmY2sxeW5tbWxvZzhpZ3VldTgwdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/EQIkgfMCjs1kk/giphy.gif"
 }
 
 }
